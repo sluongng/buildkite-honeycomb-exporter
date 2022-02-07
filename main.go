a21d19173f11e7a06d530187cfdaacb85844f570
Son Luong Ngoc <sluongng@gmail.com>
2022-02-07 11:42:02 +0100

    Implement cache and long running loop
    Added a long running loop that let this program continuously run and
    query latest data from BuildKite.
    
    In order to avoid duplication between each runs, 2 mechanisms were
    added:
    
    - A moving pointer lastFinishedAt to keep track of the latest build
      finished time stamp. Subsequent runs will only query BuildKite API
      starting from this time stamp.
    
    - A cache file keeping track of all processed Build IDs. Builds which
      have ID stored in this cache file will not be processed as trace
      again.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
 	"log"
@@ -97,14 +98,78 @@ func main() {
 	tracer, shutdown := initOtel(ctx)
 	defer shutdown()
 
-	// BuildKite pagination loop
+	sleepDuration := 15 * time.Minute
+	for {
+		// TODO: implement graceful shutdown when SIGTERM/SIGKILL
+		processBuildKite(ctx, bk, tracer)
+
+		log.Printf("sleeping for %s", sleepDuration)
+		time.Sleep(sleepDuration)
+	}
+}
+
+func loadCache(cacheFile *os.File) map[string]struct{} {
+	result := make(map[string]struct{})
+	scanner := bufio.NewScanner(cacheFile)
+	for scanner.Scan() {
+		result[scanner.Text()] = struct{}{}
+	}
+
+	fmt.Printf("loading cache: %d lines\n", len(result))
+
+	return result
+}
+
+func writeCache(cacheBuildIDs map[string]struct{}, cacheFile *os.File) error {
+	err := cacheFile.Truncate(0)
+	if err != nil {
+		return fmt.Errorf("error truncating cache: %v", err)
+	}
+	_, err = cacheFile.Seek(0, 0)
+	if err != nil {
+		return fmt.Errorf("error seeking cache: %v", err)
+	}
+
+	w := bufio.NewWriter(cacheFile)
+	defer w.Flush()
+
+	for k := range cacheBuildIDs {
+		_, err := w.WriteString(k + "\n")
+		if err != nil {
+			return fmt.Errorf("error writing cache: %v", err)
+		}
+	}
+
+	return nil
+}
+
+// Honeycomb has max data retention of 60 days
+// so we should not need to send more than that worth of data
+//
+// For subsequent runs, only query from when last run left off
+var lastFinishedAt = time.Now().AddDate(0, -2, 0)
+
+const CachePath = "/tmp/buildkite-id-cache.txt"
+
+// BuildKite pagination loop
+func processBuildKite(ctx context.Context, bk *buildkite.Client, tracer trace.Tracer) {
+	// load cache file on each run
+	f, err := os.OpenFile(CachePath, os.O_RDWR|os.O_CREATE, 0775)
+	if err != nil {
+		log.Fatalf("could not open cache file: %v\n", err)
+	}
+	defer f.Close()
+
+	cachedBuildIDs := loadCache(f)
+
+	// track what is the latest build finish time
+	runFinishedAt := lastFinishedAt
+
 	wg := &sync.WaitGroup{}
 	page := 1
 	for {
 		buildListOptions := &buildkite.BuildsListOptions{
-			// Honeycomb has max data retention of 60 days
-			// we should not need to send more than that worth of data
-			FinishedFrom: time.Now().AddDate(0, -2, 0),
+			FinishedFrom: lastFinishedAt,
 			// Possible values are: running, scheduled, passed, failed, canceled, skipped and not_run.
 			// filters for only 'finished' states
 			State: []string{"passed", "failed", "canceled", "skipped"},
@@ -122,6 +187,19 @@ func main() {
 			continue
 		}
 		for _, b := range builds {
+			if _, ok := cachedBuildIDs[*b.ID]; ok {
+				// build ID is in cache, skip processing
+				log.Println("Skipping build:", *b.ID)
+				continue
+			}
+
+			// add build ID to cache
+			cachedBuildIDs[*b.ID] = struct{}{}
+
+			if b.FinishedAt != nil && b.FinishedAt.After(runFinishedAt) {
+				runFinishedAt = b.FinishedAt.Time
+			}
+
 			wg.Add(1)
 			go processBuild(ctx, tracer, b, wg)
 		}
@@ -132,6 +210,16 @@ func main() {
 		}
 		page = resp.NextPage
 	}
+
+	// store all build IDs each run into cache
+	err = writeCache(cachedBuildIDs, f)
+	if err != nil {
+		log.Fatalf("error writing cache: %v", err)
+	}
+
+	// Update future runs query starting point so that
+	// we don't have to read 2 months worth each time.
+	lastFinishedAt = runFinishedAt
 
 	// ensure all workers are finished
 	wg.Wait()
